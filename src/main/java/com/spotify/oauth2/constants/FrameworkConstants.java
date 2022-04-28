fcee5f2d629f93676ec5e25f7b0f9ce44151a407
“rajatt95” <rajatvermaa95@gmail.com>
2022-04-29 01:57:26 +0530

    Details added in files

--- src/main/java/com/spotify/oauth2/constants/FrameworkConstants.java
+++ src/main/java/com/spotify/oauth2/constants/FrameworkConstants.java
@@ -1,3 +1,15 @@
+/**
+ * @author Rajat Verma
+ * https://www.linkedin.com/in/rajat-v-3b0685128/
+ * https://github.com/rajatt95
+ * https://rajatt95.github.io/
+ *
+ * Course: REST Assured API Automation from scratch + Framework + CI (https://www.udemy.com/course/rest-assured-api-automation/)
+ * Tutor: Omprakash Chavan (https://www.udemy.com/user/omprakash-chavan/)
+ */
+
+/***************************************************/
+
 package com.spotify.oauth2.constants;
 
 import java.util.Date;
